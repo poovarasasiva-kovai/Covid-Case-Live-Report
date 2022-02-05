2b3ebe8a77ab0fb96f90b3062676b8f94cb02571
poovarasasiva-kovai <poovarasa.siva@kovai.co>
2022-02-05 16:45:34 +0530

    Added Feature

--- Dashboard.js
+++ Dashboard.js
@@ -78,21 +78,51 @@ function load() {
       data.Countries.forEach((element) => {
         if (element.CountryCode == countries[index]) {
           document.getElementById("idTotalConfirmed").innerHTML =
-            element.TotalConfirmed;
+          changeNumberFormat(element.TotalConfirmed, 3);
           document.getElementById("idNewConfirmed").innerHTML =
-            element.NewConfirmed;
+          changeNumberFormat(element.NewConfirmed, 3);
           document.getElementById("idTotalDeaths").innerHTML =
-            element.TotalDeaths;
+          changeNumberFormat(element.TotalDeaths, 3);
         }
         document.getElementById("idTotalConfirmedG").innerHTML =
-          data.Global.TotalConfirmed;
+        changeNumberFormat(data.Global.TotalConfirmed, 3);
         document.getElementById("idNewConfirmedG").innerHTML =
-          data.Global.NewConfirmed;
+        changeNumberFormat(data.Global.NewConfirmed, 3);
         document.getElementById("idTotalDeathsG").innerHTML =
-          data.Global.TotalDeaths;
+        changeNumberFormat(data.Global.TotalDeaths, 3);
       });
       sort();
     });
+}
+
+function changeNumberFormat(number, decimals, recursiveCall) {
+  const decimalPoints = decimals || 2;
+  const noOfLakhs = number / 100000;
+  let displayStr;
+  let isPlural;
+
+  // Rounds off digits to decimalPoints decimal places
+  function roundOf(integer) {
+      return +integer.toLocaleString(undefined, {
+          minimumFractionDigits: decimalPoints,
+          maximumFractionDigits: decimalPoints,
+      });
+  }
+
+  if (noOfLakhs >= 1 && noOfLakhs <= 99) {
+      const lakhs = roundOf(noOfLakhs);
+      isPlural = lakhs > 1 && !recursiveCall;
+      displayStr = `${lakhs} Lakh${isPlural ? 's' : ''}`;
+  } else if (noOfLakhs >= 100) {
+      const crores = roundOf(noOfLakhs / 100);
+      const crorePrefix = crores >= 100000 ? changeNumberFormat(crores, decimals, true) : crores;
+      isPlural = crores > 1 && !recursiveCall;
+      displayStr = `${crorePrefix} Crore${isPlural ? 's' : ''}`;
+  } else {
+      displayStr = roundOf(+number);
+  }
+
+  return displayStr;
 }
 
 load();
